5c75e852c93f930444b77b1e443d17db6160e53a
leroyvaughan <leroy@glvconsulting.com>
2013-09-17 23:24:20 -0700

    Builder Software Design Pattern
    console app demo: this was the third evolution of the Template Method
    demo
    
    - template method
    - factory method
    - builder pattern

---
+++ TargetPractice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TargetPractice.Bubbles;
+using TargetPractice.TargetBuilder;
+
+namespace TargetPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets make some bubbles!\r\n");
+
+            //TEMPLATE METHOD DESIGN PATTERN USAGE
+            Console.WriteLine("These will be made using the Template Method");
+            Console.WriteLine("----------------------------------------\r\n");
+            BlueBubble BlueBubble = new BlueBubble();
+            GreenBubble GreenBubble = new GreenBubble();
+            RedBubble RedBubble = new RedBubble();
+
+            //FACTORY METHOD DESIGN PATTERN USAGE
+            Console.WriteLine("\r\nNow let's use the Factory Method!");
+            Console.WriteLine("----------------------------------------\r\n");
+            BubbleFactory BubblesInc = new BubbleFactory();
+            BubblesInc.CreateBubble(1);
+            BubblesInc.CreateBubble(2);
+            BubblesInc.CreateBubble(3);
+
+
+            //BUILDER DESIGN PATTERN
+            Console.WriteLine("\r\nNow let's use the Builder Design Pattern!");
+            Console.WriteLine("----------------------------------------\r\n");
+            ObjectDirector ObjDirector = new ObjectDirector();
+            ObjDirector.MakeBubble(1);      //red
+            ObjDirector.MakeBubble(2);      //green
+            ObjDirector.MakeBubble(3);      //blue
+
+            ObjDirector.MakeStarburst(1);   //black
+            ObjDirector.MakeStarburst(2);   //yellow
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TargetPractice/TargetBuilder/ObjectTemplate2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TargetPractice.TargetBuilder
+{
+    public abstract class ObjectTemplate2
+    {
+        public string _color;
+        public string _objectType;
+        public int _points;
+        public int _speed;
+
+        //these methods are abstract in ObjectTemplate1
+        public virtual void SetPointsValue(int Points){ _points = Points; }
+        public virtual void SetObjectColor(string Color){ _color = Color; }
+        public virtual void SetObjectSpeed(int Speed){ _speed = Speed; }
+
+        public void SetObjectProperties()
+        {
+            Console.WriteLine("{0} colored {1} worth {2} points falling at speed {3}.\r\n",
+                                _color, _objectType, _points, _speed);
+        }
+    }
+}
